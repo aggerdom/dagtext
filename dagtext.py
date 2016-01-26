6620ecc85b7fd468b75f0ce2905b569de7fcd61e
Alex Gerdom <alexgerdom@gmail.com>
2016-01-25 23:06:44 -0500

    added drag and drop support, recolored save buttons

--- dagtext.py
+++ dagtext.py
@@ -15,18 +15,16 @@ class TextNode(object):
         """
         self.toplevel = tk.Toplevel()
         self.topFrame = tk.Frame(self.toplevel)
-        self.topFrame.pack()
+        self.topFrame.pack(side=tk.TOP)
+        self.buttonFrame = tk.Frame(self.toplevel)
+        self.buttonFrame.pack(side=tk.TOP)
         self.textWidget = tk.Text(self.topFrame)
         self.textWidget.pack()
         self.textWidget.insert(tk.END,self.text)
-        self.saveButton = tk.Button(self.topFrame,
-                                    text="save",
-                                    command = self.on_save)
-        self.saveButton.pack()
-        self.exitButton = tk.Button(self.topFrame,
-                                    text="exit",
-                                    command = self.on_exit)
-        self.exitButton.pack()
+        self.saveButton = tk.Button(self.topFrame, text="save", command = self.on_save,bg="green" )
+        self.saveButton.pack(side=tk.LEFT,fill=tk.X,expand=True)
+        self.exitButton = tk.Button(self.topFrame, text="exit", command = self.on_exit,bg="red")
+        self.exitButton.pack(side=tk.LEFT,fill=tk.X,expand=True)
 
     def on_save(self):
         """Persists the changes to the textnode since the start of the current editting session."""
@@ -50,37 +48,53 @@ class CanvasNode(TextNode):
 class MyCanvas(tk.Canvas):
     def __init__(self,parent,*args,**kwargs):
         tk.Canvas.__init__(self,parent,*args,**kwargs)
+        self.nodes = {} # dictionary of textnodes organized by handle
         self.bind("<1>",self.make_node)
         self.bind("<2>",self.edit_node)
-        self.nodes = {}
+        self.bind("<ButtonPress-3>",self.down)
         self.current_node = None
 
     def make_node(self,event):
         node = CanvasNode(self,event.x,event.y)
-        print(node.handle)
+        # print(node.handle)
         self.nodes[node.handle]=node
-        print(self.nodes)
-
-    def demoOfCurrent(self,event):
-        """Calls the edit command on the node closest to the click event"""
-        #
-        if self.find_withtag(tk.CURRENT):
-            self.itemconfig(tk.CURRENT,fill="blue")
-            self.update_idletasks()
+        # print(self.nodes)
 
     def edit_node(self,event):
         itemID = self.find_closest(event.x, event.y)
         self.nodes[itemID[0]].edit()
 
-    def select_node(self,nodeId):
-        # deselect and decolor current node
-        if self.current_node is not None:
-            self.itemconfigure(self.current_node,fill="") # clear filling
-        self.current_node = nodeId
-        self.itemconfigure(nodeId,fill='blue')
+    def down(self,event):
+        """
+        EVENT SEQUENCE: (part 1)
+            part 1: down    - Get the node under the cursor
+            part 2: motion  - update the coordinates everytime the mouse moves until button 3 is released
+            part 3: up      - unbind part 2 events, and drop the node at its final location
+        """
+        if self.find_withtag(tk.CURRENT):
+            self.current_node = self.find_withtag(tk.CURRENT)
+            self.itemconfig(self.current_node,fill="blue")
+            self.bind("<Motion>",self.motion)
 
-    def edit_test(self):
-        self.textNode.edit()
+    def motion(self,event):
+        x1,y1,x2,y2 = self.coords(self.current_node)
+        width, height = x2-x1, y2-y1
+        newx1, newx2 = event.x - .5*width, event.x + .5*width
+        newy1, newy2 = event.y - .5*height,event.y + .5*height
+        self.coords(self.current_node,newx1,newy1,newx2,newy2)
+        self.bind("<ButtonRelease-3>",self.up)
+
+    def up(self,event):
+        # unbind everything
+        self.unbind("<Motion>")
+        self.unbind("<ButtonRelease-3>")
+        # update the coordinates at the final location (possibly unnecessary)
+        x1,y1,x2,y2 = self.coords(self.current_node)
+        width, height = x2-x1, y2-y1
+        newx1, newx2 = event.x - .5*width, event.x + .5*width
+        newy1, newy2 = event.y - .5*height,event.y + .5*height
+        self.coords(self.current_node,newx1,newy1,newx2,newy2)
+        self.itemconfig(self.current_node,fill="red")
 
 if __name__ == '__main__':
     root = tk.Tk()
