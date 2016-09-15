82cf4dfd0ae327f387f59d72e85533d46461d313
Alex Gerdom <alexgerdom@gmail.com>
2016-09-14 23:20:42 -0400

    added attributes for nodes and edges

--- dagtext/graphModel.py
+++ dagtext/graphModel.py
@@ -21,6 +21,22 @@ class DocumentGraph(object):
             graph = nx.DiGraph()
         self.G = graph
 
+    @property
+    def nodes(self):
+        """
+        :return: frozenset containing ids for all the nodes presently defined on the graph
+        :rtype: frozenset
+        """
+        return frozenset(self.G.nodes())
+
+    @property
+    def edges(self):
+        """
+        :return: (head_id,tail_id) for each of the edges on the graph
+        :rtype: frozenset
+        """
+        return frozenset(self.G.edges())
+
     def __getitem__(self, key):
         if isinstance(key, int):
             nodeid = key
@@ -40,24 +56,21 @@ class DocumentGraph(object):
                 attribs = tuple((k, v) for k, v in edgeattribs.items())
                 return Edge(head=head, tail=tail, attribs=attribs)
         else:
-            raise KeyError("DocumentGraph Instance Only Accepts nodeids (ints) and edgeids (slices) as keys")
+            raise KeyError("DocumentGraph Instance Only Accepts nodeids (ints) and edgeids (tuples) as keys")
 
     def __setitem__(self, nodeid, valuedict):
         # TODO: Implement setitem
         raise NotImplementedError
 
     def __repr__(self):
-        # TODO: FINISH REPR
-        raise NotImplementedError
-        head = ["DocumentGraph(\n"]
-        for nodeid, attributes in self.G.node.items():
-            f = ["\tNode: " + str(nodeid)]
-            for edge in self.G.edges_iter(nodeid):
-                pass
-
-        tail = []
-
-        pformat(self.G.node)
+        out = ["DocumentGraph("]
+        edges = self.edges
+        for nodeid in self.nodes:
+            out.append("\t{!r}".format(self[nodeid]))
+            for edge in filter(lambda x: x[0]==nodeid, edges):
+                out.append("\t\t{!r}".format(self[edge]))
+        out[-1] += ")"
+        return "\n".join(out)
 
     ## Methods to persist graph
     @classmethod
@@ -95,7 +108,6 @@ class DocumentGraph(object):
         nodeid = next(NODECOUNT)
         if title is None:
             title = "<Node {}>".format(nodeid)
-
         nodeattrs = dict(text=text, title=title)
         self.G.add_node(nodeid, nodeattrs)
         return nodeid
@@ -104,6 +116,10 @@ class DocumentGraph(object):
         """
         Create an directed edge from node1 to node2.
         """
+        # check that the node ids are valid for edge creation
+        assert isinstance(node1,int) and isinstance(node2,int)
+        assert(node1 in self.nodes, "Node {} is not in the graph".format(node1))
+        assert(node2 in self.nodes, "Node {} is not in the graph".format(node2))
         # TODO: Potential signal point (edge instantiation on nx graph)
         self.G.add_edge(node1, node2)
 
@@ -144,7 +160,7 @@ class DocumentGraph(object):
                              original node
 
         RETURNS:
-            (head,tail)
+            (headid, tailid)
         """
 
         text = self.G.node[nodeid]["text"]
@@ -212,3 +228,18 @@ class DocumentGraph(object):
             raise NotImplementedError
         return pos
 
+    def digraph_from_titles(self):
+        g = nx.DiGraph()
+        try:
+            assert all(isinstance(nodeid, int) for nodeid in self.nodes)
+        except AssertionError as e:
+            print(self.nodes)
+            raise e
+        node_data = [self[nodeid] for nodeid in self.nodes]  # Node named tuple
+        edge_data = [self[edgetuple] for edgetuple in self.edges]  # Edge named tuple
+        for node in node_data:
+            g.add_node(node.title)
+        for edge in edge_data:
+            g.add_edge(self[edge.head].title,
+                       self[edge.tail].title)
+        return g
--- dagtext/tests/test_graph.py
+++ dagtext/tests/test_graph.py
@@ -3,16 +3,151 @@ from mock import MagicMock
 from dagtext import graphModel
 
 
-def test_graph_instantiation():
-    graph = graphModel.DocumentGraph()
+def create_graph(elements, patch_lookup=False):
+    """
+    Instantiate a DocumentModel from a list of nodes & edges/paths.
+    Nodes should be specified as either integers or strings.
+    Paths/Edges should be provided as a tuple or list.
+
+    :param elements: iterable containing integers/strings iterable of nodes & edges
+    :type elements: list or tuple
+    :param patch_lookup: If True, will attach a reference dictionary as <instance>.reference
+    :type patch_lookup: bool
+    :return: DocumentGraph with the elements described
+    :rtype: graphModel.DocumentGraph
+
+
+    EXAMPLE
+
+        #   /-> a
+        # b -> c -> e
+        # g -> f
+
+        >>> elements = ('a', ('b','c','e'), ('g','f'))
+        >>> doc = create_graph(elements, patch_lookup=True)
+        >>> lookup = doc.lookup
+        >>> lookup
+        {'e': 21, 'b': 19, 'g': 22, 'a': 0, 'c': 20, 'f': 23}
+        >>> doc.is_dag()
+        True
+        >>> sorted(doc.edges)
+        [(7, 8), (8, 9), (10, 11)]
+        >>> g = doc.digraph_from_titles()
+        >>> sorted(g.nodes())
+        ['a','b','c','e','f','g']
+        >>> g.edges()
+
+    """
+    doc = graphModel.DocumentGraph()
+    lookup = {}
+    for element in elements:
+        # single element => node
+        # two or more elements => edge or path
+        if type(element) in (int, str):
+            lookup[element] = doc.add_node(text=str(element), title=str(element))
+        elif type(element) in (list, tuple):
+            if len(element) <= 1:
+                raise ValueError("Paths must contain at least two elements")
+
+            for head, tail in zip(element[:-1], element[1:]):
+                if any(type(part) not in (int, str) for part in (head, tail)):
+                    raise ValueError("Values w/in a path/edge must be ints or str")
+                # add the node if it hasn't occured previously
+                if head not in lookup:
+                    lookup[head] = doc.add_node(text=str(head), title=str(head))
+                if tail not in lookup:
+                    lookup[tail] =  doc.add_node(text=str(tail), title=str(tail))
+                doc.add_edge(lookup[head], lookup[tail])
+    if patch_lookup:
+        doc.lookup = lookup
+    return doc
+
+
+@pytest.fixture(scope='function')
+def reset_nodecount():
+    import itertools
+    graphModel.NODECOUNT = itertools.count()
+
+
+def test_test_create_graph(reset_nodecount):
+    elements = ('a', ('b', 'c'))
+    doc = create_graph(elements, patch_lookup=True)
+    lookup = doc.lookup
+
+    # check it creates it correctly
+    g = doc.digraph_from_titles()
+    assert all(g.has_node(n) for n in ('a', 'b', 'c'))
+    assert g.has_edge('b', 'c')
+
+    # check that it updates
+    lookup['e'] = doc.add_node(title="e", text="e")
+    doc.add_edge(lookup["b"], lookup["e"])
+    g = doc.digraph_from_titles()
+    assert g.has_node('e')
+    assert g.has_edge('b', 'e')
+
+    # check that errors are correctly thrown
+    malformed = [('a', ('b', 'c'), ()),  # empty edge
+                 ('a', ('b', 'c', 'e'), ('a',)),  # edge w/ one member
+                 ('a', ('b', ('c', 'd'))),  # edge w/ nested edge
+                 ]
+    for malexp in malformed:
+        with pytest.raises(ValueError):
+            print(malexp)
+            doc = create_graph(malexp, patch_lookup=True)
+
+
+@pytest.fixture(scope='function')
+def instantiate_graph(reset_nodecount):
+    doc = graphModel.DocumentGraph()
+    return doc
+
+
+@pytest.fixture(scope='function')
+def instantiate_dag():
+    # a -> b -> c -> e
+    #      \ -> d
+    elements = (('a', 'b', 'c', 'e'),
+                ('b', 'd'))
+    doc = create_graph(elements, patch_lookup=True)
+    lookup = doc.lookup
+    return doc, lookup
+
+
+@pytest.fixture(scope='function')
+def instantiate_nondag(instantiate_graph):
+    # a -> b -> c -> e
+    #      \ -> d -> a
+    doc = instantiate_graph
+    a = doc.add_node(title='a')
+    b = doc.add_node(title='b')
+    c = doc.add_node(title='c')
+    d = doc.add_node(title='d')
+    e = doc.add_node(title='e')
+    for edge in ((a, b), (b, c), (c, e), (b, d), (d, a)):
+        doc.add_edge(*edge)
+    return doc
+
+
+def test_dag(reset_nodecount):
+    dag = instantiate_dag
+
+
+def add_n_nodes(docgraph, n):
+    added_ids = [docgraph.add_node(text="added node") for _ in range(n)]
+    return added_ids
+
+
+def test_graph_instantiation(instantiate_graph):
+    graph = instantiate_graph
     assert hasattr(graph, "G")
     assert isinstance(graph.G, graphModel.nx.DiGraph)
 
 
-def test_node_instantiation():
+def test_node_instantiation(reset_nodecount):
     graph = graphModel.DocumentGraph()
-    node = graph.add_node(text="", title="<Node 0>")
-    assert node == 0
+    nodeids = add_n_nodes(graph, 1)
+    assert nodeids[0] == 0
 
 
 def test_split_default():
@@ -55,3 +190,10 @@ class TestGraphModelGetter:
         assert g[n1].title == 'Foo'
         assert g[n1].text + "!" == "Hello world!"
         assert g[n1].text + g[n1].text == "Hello worldHello world"
+
+    def test_edge_attribute(self, reset_nodecount):
+        doc = create_graph((('a', 'b', 'c', 'd', 'e'),)
+                           , patch_lookup=True)
+        for edge in  (('a', 'b'), ('b', 'c'), ('c', 'd'), ('d', 'e')):
+            edgeids = (doc.lookup[edge[0]], doc.lookup[edge[1]])
+            assert edgeids in doc.edges
